49965d98e0acaddfd718341e0fb0013a88ce3965
JeriBeth Stevens <jeribeth@gmail.com>
2017-03-02 19:08:54 -0500

    Initial code commit with base HTML, CSS, and JS

---
+++ app.js
@@ -0,0 +1,25 @@
+//determine # of players
+	//if 1 player then v computer, if 2 players vs each other
+//determine which player is X, O
+//begin game 
+//assign each box a number to track
+// track with array of winning combos
+//once winning combo met show winner msg
+//if no winning combo met, show cat msg
+//ask to restart game
+
+var winningCombos = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[1, 4, 7],
+	[2, 5, 8],
+	[3, 6, 9],
+	[1, 5, 9],
+	[3, 5, 7] 
+];
+
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+
